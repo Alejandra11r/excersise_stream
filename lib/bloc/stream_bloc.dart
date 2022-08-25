e1dd616d0d39e73e20f664fb10ac7ae73d115c19
Alejandra Rodriguez <alejith11rr@gmail.com>
2022-08-25 18:28:12 -0500

    Se finaliza streamController

--- lib/bloc/stream_bloc.dart
+++ lib/bloc/stream_bloc.dart
@@ -4,17 +4,14 @@ import 'dart:async';
 class StreamBloc {
 
   int a=0, b=0, valueA=0, valueB=0;
-   Map<String,int> numbers = {
-      "valueA": 0,
-      "valueB": 0,
-      "sum": 0
-    };
-  final myStreamA = StreamController();
-  final myStreamB = StreamController();
 
-
-  final suma = StreamController<Map<String,int>>();
-  Stream<Map<String,int>>  get mapaSumaStream => suma.stream;
+  final _myStreamA = StreamController<int>();
+  final _myStreamB = StreamController<int>();
+  final _myStreamSum = StreamController<int>();
+  
+  Stream<int>  get valueAStream => _myStreamA.stream;
+  Stream<int>  get valueBStream =>  _myStreamB.stream;
+  Stream<int>  get sumStream => _myStreamSum.stream;
 
 
   StreamBloc(){
@@ -29,38 +26,24 @@ class StreamBloc {
     Timer.periodic( const Duration(milliseconds: 1500), (clock){
       a += clock.tick;
       b += clock.tick;
-      myStreamA.sink.add(a);
-      myStreamB.sink.add(b);
-    });
-
-
-     myStreamA.stream.listen((event){
-      valueA = event;
-      numbers = {
-        'valueA': valueA,
-        'valueB': valueB,
-        'sum': sum()
-      };
-      suma.sink.add(numbers);
-    });
-    
-    myStreamB.stream.listen((event){
-      valueB = event;
-       numbers = {
-        'valueA': valueA,
-        'valueB': valueB,
-        'sum': sum()
-      };
-      suma.sink.add(numbers);
+      _myStreamA.sink.add(a);
+      valueA = a;
+      _myStreamB.sink.add(b);
+      valueB = b;
+      _myStreamSum.sink.add(sum(valueA, valueB));
     });
   }
    
-  int sum(){
-      return valueA + valueB;
+  int sum(a, b){
+      return a + b;
   }
-  
-  
-  
+
+  dispose(){
+    _myStreamA.close();
+    _myStreamB.close();
+    _myStreamSum.close();
+  }
 }
 
 final streamBloc = StreamBloc();
+
